cd0096711eacfa9a1d9fc65b1b16dd9572c9d957
Rafi Muhammad Daffa <43717176+skycruiser8@users.noreply.github.com>
2022-11-29 23:31:08 +0700

    Update version.php

--- version.php
+++ version.php
@@ -24,11 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020072700;
-$plugin->requires  = 2017111300;
+$plugin->version   = 2022112900;
+$plugin->requires  = 2016120500;
 $plugin->cron      = 0;
 $plugin->component = 'quizaccess_safeexambrowser';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.4 for Moodle 3.4+';
+$plugin->release   = '1.2 for Moodle 3.2+';
 
 $plugin->outestssufficient = true;
